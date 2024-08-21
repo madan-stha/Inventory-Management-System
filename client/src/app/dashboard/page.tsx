1654b301ee42c0bb7a116e62814f39f010a18bb8
madan-stha <madan@scieverinc.com>
2024-08-21 10:02:00 +0545

    added dashboard cards

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import CardExpenseSummary from "./CardExpenseSummary";
+import CardPopularProducts from "./CardPopularProducts";
+import CardPurchaseSummary from "./CardPurchaseSummary";
+import CardSalesSummary from "./CardSalesSummary";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+      <CardSalesSummary />
+      <CardPurchaseSummary />
+      <CardExpenseSummary />
+      <div className="md:row-span-1 xl:row-span-2 bg-red-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-red-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-red-500" />
+    </div>
+  );
 };
 
 export default Dashboard;
