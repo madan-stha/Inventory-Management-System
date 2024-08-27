33910f92e32168e586ad9b976b45399ca57ad757
madan-stha <madan@scieverinc.com>
2024-08-27 10:33:07 +0545

    added get products services

---
+++ client/src/app/products/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+import { useGetProductsQuery } from "@/state/api";
+import { PlusCircleIcon, Search } from "lucide-react";
+import Header from "@/app/(components)/Header";
+
+const Products = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const {
+    data: products,
+    isLoading,
+    isError,
+  } = useGetProductsQuery(searchTerm);
+
+  if (isLoading) {
+    return <div className="py-4">Loading...</div>;
+  }
+
+  if (isError || !products) {
+    return (
+      <div className="text-center text-red-500 py-4">
+        Failed to fetch products
+      </div>
+    );
+  }
+  return (
+    <div className="mx-auto pb-5 w-full">
+      {/* SEARCH BAR */}
+      <div className="mb-6">
+        <div className="flex items-center bottom-2 border-gray-200 rounded">
+          <Search className="w-5 h-5 text-gray-500 m-2" />
+          <input
+            className="w-full py-2 px-4 rounded bg-white"
+            placeholder="Search products ..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+
+      {/* HEADER BAR */}
+      <div className="flex justify-between items-center mb-6">
+        <Header name="Products" />
+        <button
+          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-2 rounded"
+          onClick={() => setIsModalOpen(true)}
+        >
+          <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
+          Product
+        </button>
+      </div>
+
+      {/* BODY PRODUCTS LIST */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between">
+        {isLoading ? (
+          <div className="">Loading</div>
+        ) : (
+          products?.map((product) => (
+            <div className="" key={product.productId}>
+              {product.productId}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
